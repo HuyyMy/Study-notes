9a21699685c57c86aba8eb5e2b0aae429f732507
huyueyang <hyyphp@163.com>
2017-11-08 09:44:06 +0800

    composer

---
+++ study/path.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hyy
+ * Date: 2017/10/30
+ * Time: 10:58
+ */
+
+    echo __FILE__;
+    echo '<br>';
+    echo dirname(__FILE__);
+    echo '<br>';
+    echo __DIR__;
+    echo '<br>';
+    var_dump($_SERVER);
+    phpinfo();
