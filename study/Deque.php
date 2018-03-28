1d78d65b7f0d381eb55114e46d0ae6a7213e5141
hyy999666 <hyyphp@163.com>
2018-03-28 11:08:40 +0800

    双向队列

---
+++ study/Deque.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Class Deque
+ *
+ * 双向队列
+ */
+
+class Deque
+{
+    private $queue = [];
+
+    /**
+     * 头入队
+     *
+     * @param $item
+     */
+    public function addFirst($item)
+    {
+        array_unshift($this->queue, $item);
+    }
+
+    /**
+     * 尾入队
+     *
+     * @param $item
+     */
+    public function addLast($item)
+    {
+        array_push($this->queue, $item);
+    }
+
+    /**
+     * 头出队
+     */
+    public function removeFirst()
+    {
+        array_shift($this->queue);
+    }
+
+    /**
+     * 尾出队
+     */
+    public function removeLast()
+    {
+        array_pop($this->queue);
+    }
+
+    public function show()
+    {
+        foreach ($this->queue as $item) {
+            echo $item . ' ';
+        }
+        echo '<br/>';
+    }
+}
+
+$deque = new Deque();
+$deque->addFirst(2);
+$deque->addLast(3);
+$deque->addLast(4);
+$deque->addFirst(5);
+$deque->show();
