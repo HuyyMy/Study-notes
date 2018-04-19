5e13fd3ebca13eb8c865c6eb1122cf10f6bb87be
hyy999666 <hyyphp@163.com>
2018-04-19 14:10:23 +0800

    add some file

---
+++ study/parseUrl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hyyph
+ * Date: 2018/4/11
+ * Time: 11:06
+ */
+
+$url = 'https://imajinyun.xyz/2018/04/05/php-error-exception-handler/#more';
+
+$arr = parse_url($url);
+
+/**
+ * array(4) {
+["scheme"]=>
+string(5) "https"
+["host"]=>
+string(13) "imajinyun.xyz"
+["path"]=>
+string(40) "/2018/04/05/php-error-exception-handler/"
+["fragment"]=>
+string(4) "more"
+}
+ */
